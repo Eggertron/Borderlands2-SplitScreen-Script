e643c3f590eb7b70887f15483c964b26f7daeeab
Egger Haaaaan <bboyhan@hotmail.com>
2019-02-09 17:57:57 -0500

    Update bl2ss.bat

--- bl2ss.bat
+++ bl2ss.bat
@@ -1,10 +1,16 @@
 set STEAMLIBFOLDER=D:\Games\SteamLibrary
+set BLFOLDER="%STEAMLIBFOLDER%\steamapps\common\Borderlands 2\Binaries\Win32"
 set BLBIN="%STEAMLIBFOLDER%\steamapps\common\Borderlands 2\Binaries\Win32\Borderlands2.exe"
 set BLXRES=884
 set BLYRES=992
+set BLARGS=-ResX=%BLXRES% -ResY=%BLYRES% -NoLauncher -AlwaysFocus -WindowPosY=0
+set BLARGS1=%BLARGS% -WindowsPosX=0
+set BLARGS2=%BLARGS% -WindowsPosX=%BLXRES% -ControllerOffset=1 -SaveDataId=2
 echo "Loading the First Player on the the left screen"
-%BLBIN% -ResX=%BLXRES% -ResY=%BLYRES% -WindowedFullscreen -AlwaysFocus -WindowPosX=0 -WindowPosY=0
+D:
+cd %BLFOLDER%
+start Borderlands2.exe %BLARGS1%
 
 pause
 echo "Loading the Secod Player on the Right screen"
-%BLBIN% -ResX=%BLXRES% -ResY=%BLYRES% -WindowedFullscreen -AlwaysFocus -WindowPosX=%BLXRES% -WindowPosY=0 -ControllerOffset=1 -SaveDataId=2
+start Borderlands2.exe %BLARGS2%
